ff5111c14689b14539510323ad8253e3bb95b25e
Jimductive <jdeuez@myges.fr>
2023-01-05 19:41:57 +0100

    Probably final push

--- start_project.sh
+++ start_project.sh
@@ -17,8 +17,11 @@ eval "terraform apply --auto-approve $debug"
 echo "Sleep 30s for servers to spawn ..."
 sleep 30s
 
+export DB_HOST=$(terraform output -raw cluster_uri | cut -d@ -f2 | cut -d/ -f1)
+export DB_PASSWORD=$(terraform output -raw user_password)
+
 echo "Playing Ansible playbooks, take a cofee and relax :) ..."
-eval "ansible-playbook /home/eductive09/Linux_Project/ansible/servers_config.yml -i /home/eductive09/Linux_Project/ansible/inventory.yml --ssh-common-args='-o StrictHostKeyChecking=no' $debug"
+eval "ansible-playbook /home/eductive09/Linux_Project/ansible/servers_config.yml -i /home/eductive09/Linux_Project/ansible/inventory.yml --extra-vars 'DB_PASSWORD=$DB_PASSWORD DB_HOST=$DB_HOST' --ssh-common-args='-o StrictHostKeyChecking=no' $debug"
 
 echo "All done !"
 
